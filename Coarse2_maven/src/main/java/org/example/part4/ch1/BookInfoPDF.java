31ebdb0c8fdc8b5575330636b5ab543d032a8baa
JeongEgg <wibalislfkas@naver.com>
2024-03-18 23:05:20 +0900

    10 commit

--- Coarse2_maven/src/main/java/org/example/part4/ch1/BookInfoPDF.java
+++ Coarse2_maven/src/main/java/org/example/part4/ch1/BookInfoPDF.java
@@ -1,2 +1,40 @@
-package org.example.part4.ch1;public class BookInfoPDF {
+package org.example.part4.ch1;
+
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Year;
+import java.util.HashMap;
+
+public class BookInfoPDF {
+    public static void main(String[] args) throws IOException {
+        HashMap<String, String> bookInfo = new HashMap<>();
+        bookInfo.put("title","한글 자바");
+        bookInfo.put("author","홍길동");
+        bookInfo.put("publisher","한글 출판사");
+        bookInfo.put("year",String.valueOf(Year.now().getValue()));
+        bookInfo.put("price","25000");
+        bookInfo.put("pages","400");
+
+        PdfWriter writer = new PdfWriter(new FileOutputStream("book_information.pdf"));
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+
+        PdfFont font = PdfFontFactory.createFont("NanumGothicLight.TTF", PdfEncodings.IDENTITY_H,true);
+        document.setFont(font);
+
+        for (String key : bookInfo.keySet()){
+            Paragraph paragraph = new Paragraph(key+": "+bookInfo.get(key));
+            document.add(paragraph);
+        }
+        document.close();
+        System.out.println("book_information.pdf 파일이 생성되었습니다.");
+    }
 }
--- Coarse2_maven/src/main/java/org/example/part4/ch1/ExcelExample.java
+++ Coarse2_maven/src/main/java/org/example/part4/ch1/ExcelExample.java
@@ -1,2 +1,64 @@
-package org.example.part4.ch1;public class ExcelExample {
+package org.example.part4.ch1;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ExcelExample {
+    // 1
+    public static void main(String[] args) throws IOException {
+        FileInputStream file = new FileInputStream(new File("example.xlsx"));
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (Row row : sheet){
+            for (Cell cell : row){
+                switch (cell.getCellType()){
+                    case NUMERIC:
+                        if (DateUtil.isCellDateFormatted(cell)){
+                            Date dateValue = cell.getDateCellValue();
+                            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                            String formattedDate = dateFormat.format(dateValue);
+                            System.out.println(formattedDate+"\t");
+                        }else {
+                            double numbericValue = cell.getNumericCellValue();
+                            if (numbericValue == Math.floor(numbericValue)){
+                                int intValue = (int) numbericValue;
+                                System.out.println(intValue+"\t");
+                            }else {
+                                System.out.println(numbericValue+"\t");
+                            }
+                        }
+                        break;
+                    case STRING:
+                        String stringValue = cell.getStringCellValue();
+                        System.out.println(stringValue+"\t");
+                        break;
+                    case BOOLEAN:
+                        boolean booleanValue = cell.getBooleanCellValue();
+                        System.out.println(booleanValue+"\t");
+                        break;
+                    case FORMULA:
+                        String formulaValue = cell.getCellFormula();
+                        System.out.println(formulaValue+"\t");
+                        break;
+                    case BLANK:
+                        System.out.println("\t");
+                        break;
+                    default:
+                        System.out.println("\t");
+                        break;
+                }
+            }
+            System.out.println();
+        }
+        file.close();
+        System.out.println("엑셀에서 데이터 읽어오기 성공.");
+    }
 }
